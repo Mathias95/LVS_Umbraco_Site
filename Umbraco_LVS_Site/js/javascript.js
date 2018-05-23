9800279c549e8a80084477c2a8e9d842b9fe233a
Mathias95 <mathiasrjacobsen@gmail.com>
2018-05-23 10:45:27 +0200

    Aktivitet pages changed

--- Umbraco_LVS_Site/js/javascript.js
+++ Umbraco_LVS_Site/js/javascript.js
@@ -39,15 +39,20 @@ $(document).ready(function(e) {
 });
 
 //ACTIVITY LIST/GALLERY VIEW
+$("#list-view").click(function () {
+	$(".grid-view").addClass("list-view").removeClass("grid-view"),
+		$(".item-grid-wrapper").addClass("item-list-wrapper").removeClass("item-grid-wrapper"),
+		$(".activity-grid-item").addClass("activity-list-item").removeClass("activity-grid-item"),
+		$(".overlay").addClass("no-overlay").removeClass("overlay"),
+		$(".activity-title").removeClass("overlay-wrapper"),
+		$(".grid-activity-content").addClass("list-activity-content").removeClass("grid-activity-content");
 
-$("#list-button").click(function(){
-	$("#gallery-view").hide();
-	$("#list-view").show();
-	
 });
-
-$("#gallery-button").click(function(){
-	$("#gallery-view").show();
-	$("#list-view").hide();
-	
-}); 
+$("#grid-view").click(function () {
+	$(".list-view").addClass("grid-view").removeClass("list-view"),
+		$(".item-list-wrapper").addClass("item-grid-wrapper").removeClass("item-list-wrapper"),
+		$(".activity-list-item").addClass("activity-grid-item overlay").removeClass("activity-list-item no-overlay"),
+		$(".no-overlay").addClass("overlay").removeClass("no-overlay"),
+		$(".activity-title").addClass("overlay-wrapper"),
+		$(".list-activity-content").addClass("grid-activity-content").removeClass("list-activity-content");
+});
